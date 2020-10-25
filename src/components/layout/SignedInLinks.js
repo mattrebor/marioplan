38038fd04536d6b442a7e2eb44c35e02fbf69113
Robert Tam <robert.tam+git@gmail.com>
2020-10-24 23:01:50 -0400

    Make initials in navbar read from profile

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,12 +4,13 @@ import {connect} from 'react-redux';
 import {signOut} from '../../store/actions/authActions';
 
 const SignedInLinks = (props) => {
+  const {profile} = props;
   return (
     <div>
       <ul className="right">
         <li><NavLink to='/create'>New Project</NavLink></li>
         <li><a onClick={props.signOut}>Log Out</a></li>
-        <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
+        <li><NavLink to='/' className="btn btn-floating pink lighten-1">{props.profile.initials}</NavLink></li>
       </ul>
     </div>
   )
